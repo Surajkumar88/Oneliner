bb44ee62039c406c31d72ae62a7f460278f93482
Suraj Kumar <asurajkumar.88@gmail.com>
2023-12-15 14:09:13 +0530

    v0.1 fixes in path creation

--- oneliner_app.sh
+++ oneliner_app.sh
@@ -2,8 +2,8 @@
 
 
 read -p "Enter the full path to your file:" file_path
-#file_name="/Users/suraj.kumar/Documents/bash_learn/test2bash.sh"
-tmp_file_path="/Users/suraj.kumar/Documents/bash_learn/one_line_tmp.sh"
+tmp_file_name="one_line_tmp.sh"
+tmp_file_path=$(pwd)"/"$tmp_file_name
 
 tmp=$(cat $file_path | awk '{$1=$1;print}'); echo -n "$tmp" > $tmp_file_path
 IFS=$'\n'
@@ -17,5 +17,3 @@ rm -rf "$tmp_file_path"
 #echo "$size"
 final="${final[@]:13:$size-15}"
 echo "$final" | pbcopy
-
-
